03b91a547fc2a02e2623e1075cb5e7a064874244
Ryan-Millard <millardryandevon@gmail.com>
2024-12-06 02:05:18 +0200

    Update navbar css

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,56 +1,57 @@
 import React from "react";
 import { FaHome, FaEnvelope, FaInfoCircle, FaBox, FaBriefcase, FaAngleDoubleRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import "./Navbar.css";
+import { NavLink, Link } from "react-router-dom";
+import DoubleRightArrowIcon from './DoubleRightArrowIcon';
+import styles from "./Navbar.module.css";
+import logo from '../../assets/42nd-precinct-logo.png';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-container">
-      <ul className="navbar-nav">
-        <li className="logo">
-          <Link to="/" className="nav-link">
-            <div className="logo-content">
-              <span className="logo-text">
-                <div>42nd</div>
-                <div>Precinct</div>
-              </span>
-            </div>
-            <FaAngleDoubleRight className="arrow-icon" id="FaAngleDoubleRight" />
-          </Link>
-        </li>
-        <li className="nav-item" id="themeButton">
-          <Link to="/" className="nav-link">
-            <FaHome aria-label="Home" />
-            <span className="link-text">Home</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-link">
-            <FaEnvelope aria-label="Contact Us" />
-            <span className="link-text">Contact Us</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            <FaInfoCircle aria-label="About Us" />
-            <span className="link-text">About Us</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/products-and-services" className="nav-link">
-            <FaBox aria-label="Products and Services" />
-            <span className="link-text">Products & Services</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/careers" className="nav-link">
-            <FaBriefcase aria-label="Careers" />
-            <span className="link-text">Employment Opportunities</span>
-          </Link>
-        </li>
-        <li className="bottom-spacer"></li>
-      </ul>
-    </nav>
+	<nav className={styles.navbar}>
+	  <ul className={styles.navbarNav}>
+		{/* Logo Section */}
+		<li className={styles.logo}>
+		  <Link to="/" className={styles.navLink}>
+			<div className={styles.logoContent}>
+				<span className={styles.logoImgContainer}>
+					<img src={logo} alt="Logo" />
+					<span>
+						<p>Honesty </p><p>Integrity </p><p>Trust</p>
+					</span>
+				</span>
+			</div>
+			<DoubleRightArrowIcon className={styles.arrowIcon} />
+		 </Link>
+		</li>
+
+		{/* Navigation Items */}
+		<NavItem to="/" icon={<FaHome size={50} />} label="Home" />
+		<NavItem to="/contact" icon={<FaEnvelope size={50} />} label="Contact Us" />
+		<NavItem to="/about" icon={<FaInfoCircle size={50} />} label="About Us" />
+		<NavItem to="/products-and-services" icon={<FaBox size={50} />} label="Products & Services" />
+		<NavItem to="/careers" icon={<FaBriefcase size={50} />} label="Employment Opportunities" />
+
+		{/* Spacer for bottom */}
+		<li className={styles.bottomSpacer}></li>
+	  </ul>
+	</nav>
+  );
+};
+
+// NavItem component to reduce redundancy
+const NavItem = ({ to, icon, label }) => {
+  return (
+	<li className={styles.navItem}>
+	  <NavLink
+		to={to}
+		className={({ isActive }) =>
+		  isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+		}
+	  >
+		{icon}
+		<span className={styles.linkText}>{label}</span>
+	  </NavLink>
+	</li>
   );
 };
 
