c61e049b6a04b0f09bb2cdf6025e7fc23666ca61
Ryan-Millard <millardryandevon@gmail.com>
2024-12-17 17:29:58 +0200

    Add hotline to header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { GiPoliceCar } from "react-icons/gi";
 import styles from './Header.module.css';
 import logo from '../../assets/42nd-precinct-logo.png';
 
 const Header = () => {
-  return (
-    <header className={styles.headerWrapper}>
-      <a href="/">
-        <img className={styles.logo} src={logo} alt="logo" />
-      </a>
-    </header>
-  );
+	return (
+		<header className={styles.headerWrapper}>
+			<Link to="/">
+				<img className={styles.logo} src={logo} alt="logo" />
+			</Link>
+			<a href="tel:0117916340" style={{ textDecoration: "none", color: "inherit" }}>
+				<figure style={{ display: "inline-block", textAlign: "center" }}>
+					<GiPoliceCar className={styles.icon} />
+				</figure>
+			</a>
+		</header>
+	);
 };
 
 export default Header;
+
