501e6e9fa98bb7d1f2af2030220378e520842a24
Ryan-Millard <millardryandevon@gmail.com>
2024-12-17 18:32:24 +0200

    Create theme toggle button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { GiPoliceCar } from "react-icons/gi";
-import styles from './Header.module.css';
-import logo from '../../assets/42nd-precinct-logo.png';
+import ThemeToggle from './ThemeToggle';
+
+import styles from "./Header.module.css";
+import logo from "@assets/42nd-precinct-logo.png";
 
 const Header = () => {
 	return (
 		<header className={styles.headerWrapper}>
+			<ThemeToggle />
+
 			<Link to="/">
 				<img className={styles.logo} src={logo} alt="logo" />
 			</Link>
+
 			<a href="tel:0117916340" style={{ textDecoration: "none", color: "inherit" }}>
 				<figure style={{ display: "inline-block", textAlign: "center" }}>
 					<GiPoliceCar className={styles.icon} />
@@ -20,4 +24,3 @@ const Header = () => {
 };
 
 export default Header;
-
