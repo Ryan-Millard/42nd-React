af4d9462cad38fc076675c941bb219c635580ea8
Ryan-Millard <millardryandevon@gmail.com>
2024-12-24 00:29:47 +0200

    Move heading styles to section h2 in App.css

--- src/Pages/Home/Sections/MissionStatement/MissionStatement.jsx
+++ src/Pages/Home/Sections/MissionStatement/MissionStatement.jsx
@@ -5,7 +5,7 @@ import styles from './MissionStatement.module.css'
 
 const MissionStatement = () => (
 	<section className="section">
-		<h2 className={styles.heading}>Mission Statement</h2>
+		<h2>Mission Statement</h2>
 		<p className={`section-text ${styles.pText}`}>To grow organically and promote innovation. We will deliver services to our clients upholding core values of Honesty, Integrity, and Trust (HIT). In addition, we will strive to become recognized as a reliable leading supplier of quality pro-active security services.</p>
 
 		<Suspense fallback={<div className={styles.businessCardFallback}>Loading...</div>}>
